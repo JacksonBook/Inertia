f01b7151c83ba53c11139225173864606b2a351a
JacksonBook <jackson.book@students.aie.edu.au>
2015-11-19 14:03:52 +1030

    project updated 19/11/15 - 2:00pm

---
+++ Unity Project/Inertia/Assets/Scripts/Player1_Movement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1_Movement : MonoBehaviour {
+	
+	public float speed;
+	public float rotateSpeed;
+	
+	Rigidbody rigid;
+	
+	void Start ()
+	{
+		rigid = gameObject.GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		//Left Joystick
+		
+		if (Input.GetAxis ("Vertical")>0) {
+			rigid.AddForce (Vector3.forward * speed * Time.deltaTime);
+		} 
+		if (Input.GetAxis ("Vertical")<0) {
+			rigid.AddForce(-Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetAxis ("Horizontal")>0) {
+			rigid.AddForce(Vector3.right * speed * Time.deltaTime);
+		}
+		if (Input.GetAxis ("Horizontal")<0) {
+			rigid.AddForce(-Vector3.right * speed * Time.deltaTime);
+		}
+
+
+		//Right Joystick
+		//rotate right
+		if (Input.GetAxis ("Horizontal_RightStick_Player1")>0) {
+			transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+		//rotate left
+		if (Input.GetAxis ("Horizontal_RightStick_Player1")<0) {
+			transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+		
+		
+
+	}
+}
